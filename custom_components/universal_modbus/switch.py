5ce4a37844ef0bb5a8d9977f33f24368ddb703a5
Idel <idel@idel-online.de>
2026-07-17 04:56:19 +0200

    Fix switch write calls

--- custom_components/universal_modbus/switch.py
+++ custom_components/universal_modbus/switch.py
@@ -1,6 +1,8 @@
 """Switch platform for Universal Modbus."""
 from __future__ import annotations
 
+from typing import Any
+
 from homeassistant.components.switch import SwitchEntity
 
 from .entity import UniversalModbusEntity
@@ -16,12 +18,18 @@ async def async_setup_entry(hass, entry, async_add_entities) -> None:
 
 
 class UniversalModbusSwitch(UniversalModbusEntity, SwitchEntity):
+    """Representation of a writable Modbus switch."""
+
     @property
     def is_on(self) -> bool:
         return bool(self.coordinator.data.get(self.definition.key))
 
-    async def async_turn_on(self, **kwargs) -> None:
-        await self.coordinator.async_write(self.definition, self.definition.command_on)
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        await self.coordinator.async_write_value(
+            self.definition, self.definition.command_on
+        )
 
-    async def async_turn_off(self, **kwargs) -> None:
-        await self.coordinator.async_write(self.definition, self.definition.command_off)
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        await self.coordinator.async_write_value(
+            self.definition, self.definition.command_off
+        )
